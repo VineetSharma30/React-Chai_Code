73a7e6856808f834a8300f7904a2537186822adc
Vineet <imvineet.sh@gmail.com>
2025-09-26 02:38:04 +0530

    Learn about appwrite, environment variables - how to manage them

---
+++ 12_Mega_Blog/src/conf/config.js
@@ -0,0 +1,8 @@
+const conf={
+    appwriteurl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwrriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwrriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+export default conf
