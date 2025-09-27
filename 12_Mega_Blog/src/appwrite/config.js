089187beceb4b29be28b81c2bbb69323a264301c
Vineet <imvineet.sh@gmail.com>
2025-09-27 19:04:14 +0530

    Setup configuration for appwrite databases

--- 12_Mega_Blog/src/appwrite/config.js
+++ 12_Mega_Blog/src/appwrite/config.js
@@ -27,10 +27,103 @@ export class Services{
                     featuredImage,
                     status,
                     userId 
-                }
-            )
+                },
+            );
         }catch(error){
-            console.log("Appwrite  service :: createPost :: error ",error)
+            console.log("Appwrite  service :: createPost :: error ",error);
         }
+    }
+
+    async updatePost(slug, {title, content, featuredImage, status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.DatabaseId,
+                conf.CollectionId,
+                slug,
+                {
+                    title, 
+                    content, 
+                    featuredImage, 
+                    status
+                },
+            );
+        } catch (error) {
+            console.log("Appwrite  service :: updatePost :: error ",error);
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.DatabaseId,
+                conf.CollectionId,
+                slug
+            );
+            return true;
+        } catch (error) {
+            console.log("Appwrite  service :: deletePost :: error ",error);
+            return false; 
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.DatabaseId,
+                conf.CollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite  service :: getPost :: error ",error);
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status","active")] ){
+        try {
+            return await this.databases.listDocuments(
+                conf.DatabaseId,
+                conf.CollectionId,
+                queries,
+            )
+        } catch (error) {
+            console.log("Appwrite  service :: getPosts :: error ",error);
+            return false;
+        }
+
+    }
+
+    // file upload services
+
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.BucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite  service :: uploadFile :: error ",error);
+            return false;
+        }
+    }
+
+    async deleteFile(fileID){
+        try {
+            await this.bucket.deleteFile(
+                conf.BucketId,
+                fileID
+            )
+        } catch (error) {
+            console.log("Appwrite  service :: deleteFile :: error ",error);       
+            return false;     
+        }
+    }
+
+    getFilePreview(fileID){
+        return this.bucket.getFilePreview(
+            conf.BucketId,
+            fileID
+        )
     }
 }
