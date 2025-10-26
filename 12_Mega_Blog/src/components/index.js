7899c4c61639c16ffbe0c17c87e25cf49888a9ed
Vineet <imvineet.sh@gmail.com>
2025-10-26 23:22:01 +0530

    Completed Pages and updated main.jsx with router provider

--- 12_Mega_Blog/src/components/index.js
+++ 12_Mega_Blog/src/components/index.js
@@ -4,11 +4,27 @@ import Container from "./container/container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn"
 import RTE from "./RTE";
+import Signup from './Signup'
+import Login from './Login'
+import Input from './Input'
+import PostCard from './PostCard'
+import PostForm from './post-form/PostForm'
+import Select from './Select'
+import Button from './Button'
+import AuthLayout from './AuthLayout'
+
 export { 
     Header, 
     Footer, 
     Container, 
     Logo, 
     LogoutBtn,
-    RTE
+    RTE,
+    Signup,
+    Input,
+    PostCard,
+    PostForm,
+    Select,
+    Button,
+    AuthLayout
 }
